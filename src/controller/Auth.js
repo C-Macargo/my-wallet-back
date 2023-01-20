1d754aaa97cfe023e62b55dac7fecba53ca09ef2
C-Macargo <caio.camargos97@gmail.com>
2023-01-20 09:51:08 -0300

    feat, sign up

--- src/controller/Auth.js
+++ src/controller/Auth.js
@@ -1,12 +1,20 @@
 import db from "../config/database.js";
+import { signUpSchema } from '../model/AuthSchema.js'
 
 export async function signUp(req, res) {
-  const { name, email} = req.body;
+
+  const { name, email , password, confirmPassword} = req.body;
+  const { error } = signUpSchema.validate({ name, email, password, confirmPassword })
+
+  if (error) {
+    const errorMessage = error.details.map(err => err.message)
+    return res.status(422).send(errorMessage)
+  }
 
   try {
     await db
       .collection("users")
-      .insertOne({name, email});
+      .insertOne({name, email, password});
     res.status(201).send("Usuário cadastrado com sucesso!");
   } catch (error) {
     res.status(500).send(error.message);
